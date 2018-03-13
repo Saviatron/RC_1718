1dac190c1aab22e409a8a148747bf4d8bac936b6
Javier Martin <javier.martin816@alu.uhu.es>
2018-03-13 10:53:36 +0100

    Modificacion resta_peano.pl

--- resta_peano.pl
+++ resta_peano.pl
@@ -1,6 +1,11 @@
-% suma(?X, ?Y, ?Z)
-% es cierto cuando Z unifica con la suma
-% de X e Y en aritmética de Peano.
+% resta(?X, ?Y, ?Z)
+% es cierto cuando Z unifica con la resta
+% de X menos Y en aritmética de Peano.
+% X tiene que ser mayor que Y.
+
+% Principio de Inducción.
+% P(no)
+% P(n-1) -> P(n)
 
 resta(X, 0, X).
 resta(X, s(Y), Z) :- resta(X, Y, s(Z)).
