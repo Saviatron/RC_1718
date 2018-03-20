2cae97cab371b02f1f36a3af942d67e7b375de2b
Javier Martin <javier.martin816@alu.uhu.es>
2018-03-20 12:12:20 +0100

    elemento_enesimo.pl

--- 2- Listas/elem_enesimo.pl
+++ 2- Listas/elem_enesimo.pl
@@ -2,5 +2,5 @@
 % Es cierto cuando Elem unifica con el elemento que ocupa 
 % la posición Pos dentro de Lista.
 
-elemento_enesimo(1, [Cabeza|_], Cabeza).
+elemento_enesimo(0, [Cabeza|_], Cabeza).
 elemento_enesimo(Pos, [_|Resto], Salida) :- Pos2 is Pos - 1, elemento_enesimo(Pos2, Resto, Salida).
