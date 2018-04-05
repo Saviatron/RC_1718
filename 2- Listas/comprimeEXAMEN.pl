ff319ebd44cd6e36f3a0ba23bfbcf988518c9df2
Javier Martin <javier.martin816@alu.uhu.es>
2018-04-05 10:28:47 +0200

    Algoritmos_ord.pl

--- 2- Listas/comprimeEXAMEN.pl
+++ 2- Listas/comprimeEXAMEN.pl
@@ -1,3 +1,23 @@
 %--------------------------------------------------
-% comprime([a,a,a,b,b,c], R).
-% R=[(a,3),(b,2),(c,1)]
+%
+% comprime(+Lista, -R)
+%
+%	Es cierto cuando R unifica con una lista 
+%	que contiene los elementos de Lista
+%	en formato comprimido:
+%
+%	Ejemplo:
+%	comprime([a,a,a,b,b,c], R).
+%	R=[(a,3),(b,2),(c,1)]
+%
+%----------------------------------------------------
+
+comprime([],[]).
+comprime([E], [(E,1)]).
+comprime([Cab, Cab|Resto], [(Cab, N2)|R]) :-
+	comprime([Cab|Resto], [(Cab, N)|R]),
+	N2 is N + 1.
+
+comprime([Cab1, Cab2|Resto], [(Cab1, 1)|R]) :-
+	Cab1 \= Cab2,
+	comprime([Cab2|Resto], R).
